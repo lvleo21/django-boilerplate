d5acb6182d86832d6602dcdbb6598116a0013f62
Leonardo Veras <verasleonardo210@gmail.com>
2025-11-16 09:20:48 -0300

    ⚰️ remove: unused UUIDS3Storage

--- apps/core/storages.py
+++ apps/core/storages.py
@@ -1,6 +1,3 @@
-import os
-import uuid
-
 from storages.backends.s3boto3 import (
     S3Boto3Storage,
     S3StaticStorage
@@ -15,10 +12,3 @@ class StaticRootS3BotoStorage(S3StaticStorage):
 
 class MediaRootS3BotoStorage(S3Boto3Storage):
     location = "media"
-
-
-class UUIDS3Storage(S3Boto3Storage):
-
-    def get_valid_name(self, name):
-        filename, extension = os.path.splitext(name)
-        return f"{uuid.uuid4()}{extension.lower()}"
